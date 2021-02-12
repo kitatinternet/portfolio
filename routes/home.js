214e450c88d9527a8491d979d464a235237ce307
Chun Kit Lam <78129679+kitatinternet@users.noreply.github.com>
2021-02-12 13:11:35 -0500

    done v01

--- routes/home.js
+++ routes/home.js
@@ -1,40 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', 
-  { title: 'Home' });
-});
+let fName = '';
+let lName = '';
+let eMail = '';
+let pNumber = '';
+let message = '';
+let sPost = '';
+
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('home', 
-  { title: 'Home' });
+  { title: 'Home' ,
+    fName:fName,
+    lName:lName,
+    eMail:eMail,
+    pNumber:pNumber,
+    message:message,
+    sPost:sPost
+    });
+    sPost = '';
 });
 
-/* GET aboutMe page. */
-router.get('/aboutMe', function(req, res, next) {
-  res.render('index', 
-  { title: 'About Me' });
-});
 
-/* GET projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', 
-  { title: 'Projects' });
-});
-
-/* GET services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', 
-  { title: 'Services' });
-});
-
-/* GET contactMe page. */
-router.get('/contactMe', function(req, res, next) {
-  res.render('index', 
-  { title: 'Contact Me' });
-});
 
 module.exports = router;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+let fName = '';
+let lName = '';
+let eMail = '';
+let pNumber = '';
+let message = '';
+let sPost = '';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  { title: 'Home' });
+  res.render('home', 
+  { title: 'Home',
+  fName:fName,
+  lName:lName,
+  eMail:eMail,
+  pNumber:pNumber,
+  message:message,
+  sPost:sPost  });
+  sPost = '';
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('home', 
-  { title: 'Home' });
+  { title: 'Home',
+    fName:fName,
+    lName:lName,
+    eMail:eMail,
+    pNumber:pNumber,
+    message:message,
+    sPost:sPost
+   });
+   sPost = '';
 });
 
 /* GET aboutMe page. */
@@ -35,6 +57,19 @@ router.get('/services', function(req, res, next) {
 router.get('/contactMe', function(req, res, next) {
   res.render('index', 
   { title: 'Contact Me' });
+});
+
+/* Post contactMe page. */
+router.post('/contactMe', function(req, res, next) {
+  fName = req.body.inputFirstName;
+  lName = req.body.inputLastName;
+  eMail = req.body.inputEmail;
+  pNumber = req.body.inputPhoneNumber;
+  message = req.body.inputMessage;
+  sPost = req.body.formStates;
+  //console.log('fName:' + fName);
+  res.redirect('/home');
+
 });
 
 module.exports = router;
