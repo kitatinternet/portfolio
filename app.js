c2582520c41161b86d9d06360361d77e2b4b2113
Chun Kit Lam <78129679+kitatinternet@users.noreply.github.com>
2021-02-12 05:44:37 -0500

    finished home

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
+
+let homeRouter = require('./routes/home');
 let usersRouter = require('./routes/users');
 
 let app = express();
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{ title: 'Error'});
 });
 
 module.exports = app;
